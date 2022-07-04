e535b56b3cc398d87d72b9e5ddbacd8eea3100f1
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-04 18:57:08 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,93 @@
 <?php
+// Initialize the session
 session_start();
+
+// // Check if the user is already logged in, if yes then redirect him to welcome page
+// if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+//     header("location: welcome.php");
+//     exit;
+// }
+
+// Include config file
+require_once "/MAMP/htdocs/sql-user-authentication-app/src/include/config.php";
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = $login_err = "";
+
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Check if username is empty
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter username.";
+    } else {
+        $username = trim($_POST["username"]);
+    }
+
+    // Check if password is empty
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter your password.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate credentials
+    if (empty($username_err) && empty($password_err)) {
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            // Set parameters
+            $param_username = $username;
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Store result
+                mysqli_stmt_store_result($stmt);
+
+                // Check if username exists, if yes then verify password
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
+                            // Password is correct, so start a new session
+                            session_start();
+
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+
+                            // Redirect user to welcome page
+                            header("location: /MAMP/htdocs/sql-user-authentication-app/src/include/header.php");
+                        } else {
+                            // Password is not valid, display a generic error message
+                            $login_err = "Invalid username or password.";
+                        }
+                    }
+                } else {
+                    // Username doesn't exist, display a generic error message
+                    $login_err = "Invalid username or password.";
+                }
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // Close connection
+    mysqli_close($link);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +96,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="The aim of this project is to make a OOP based booking app in PHP">
-    <title>Library</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/sql-user-authentication-app/src/css/style.css">
     <link rel="shortcut icon" href="/sql-user-authentication-app/src/images/icon.png" type="image/x-icon">
@@ -19,40 +106,35 @@ session_start();
 </head>
 
 <body>
-    <?php
-    // Header
-    include("/MAMP/htdocs/sql-user-authentication-app/src/include/header.inc.php");
+    <main>
+        <div class="wrapper">
+            <h2>Login</h2>
+            <p>Please fill in your credentials to login.</p>
 
-    // Switch Router
-    $request = $_SERVER['REQUEST_URI'];
-    $basepath = "sql-user-authentication-app/";
-    $request = str_replace($basepath, "", $request);
-    $request = strtok($request, '?');
-    switch ($request) {
-        case '/':
-            require __DIR__ . '/src/test/login.php';
-            break;
-        case '':
-            require __DIR__ . '/src/test/login.php';
-            break;
-        case '/login':
-            require __DIR__ . '/src/test/login.php';
-            break;
-        case '/register':
-            require __DIR__ . '/src/test/register.php';
-            break;
-            case '/welcome':
-                require __DIR__ . '/src/test/welcome.php';
-                break;
-        default:
-            http_response_code(404);
-            echo "page not found";
-            break;
-    };
+            <?php
+            if (!empty($login_err)) {
+                echo '<div class="alert alert-danger">' . $login_err . '</div>';
+            }
+            ?>
 
-    // Footer
-    include("/MAMP/htdocs/sql-user-authentication-app/src/include/footer.inc.php");
-    ?>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                    <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </div>
+                <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
