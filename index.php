74c9fe0fa54533381f262500641031aa2ec1fd6b
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-04 11:31:53 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The aim of this project is to make a OOP based booking app in PHP">
+    <title>Library</title>
+    <link rel="stylesheet" href="/sql-user-authentication-app/src/css/style.css">
+    <link rel="shortcut icon" href="/sql-user-authentication-app/src/images/icon.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/e4ad388285.js" crossorigin="anonymous" defer></script>
+    <script src="/php-oop-booking-app/src/js/script.js" defer></script>
+</head>
+
+<body>
+    <!-- Header Module -->
+    <header class="header">
+        <!-- Website Logo -->
+        <a href="/sql-user-authentication-app/"><img src="/sql-user-authentication-app/src/images/airdnd.png" alt="Airdnd logo" class="logo" title="Home"></a>
+
+        <!-- Social Media -->
+        <div class="social-media">
+            <a href="https://www.linkedin.com/in/oliver-vermeulen-311221222/" target="_blank" class="media-icon" title="Linkedin"><i class="fa fa-linkedin-square" alt="Linkedin"></i></a>
+            <a href="https://github.com/OliverVermeulen" target="_blank" class="media-icon" title="GitHub"><i class="fa fa-github-square" alt="GitHub"></i></a>
+        </div>
+    </header>
+</body>
+
+</html>
