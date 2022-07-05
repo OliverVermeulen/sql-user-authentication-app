304daf91bc986d714f17a9102c01399c4bc57ae0
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-05 12:18:51 +0200

    large update

--- src/include/header.inc.php
+++ src/include/header.inc.php
@@ -1,22 +1,25 @@
 <!-- Header Module -->
 <header class="header">
     <!-- Website Logo -->
-    <a href="/sql-user-authentication-app/index.php"><img src="/sql-user-authentication-app/src/images/library-logo.png" alt="Airdnd logo" class="logo" title="Home"></a>
+    <a href="/sql-user-authentication-app/index.php"><img src="/sql-user-authentication-app/src/images/library-logo.png" alt="library logo" class="logo img-thumbnail" title="Home"></a>
 
     <!-- Search Bar -->
-    <form action="" method="post">
-        <input type="search" name="" id="" placeholder="Search">
-        <button type="submit"><i class="fa fa-search"></i></button>
-        <i class="fa fa-magnifying-glass"></i>
+    <form class="row g-2">
+        <div class="col-auto">
+            <input type="search" name="search" class="form-control" placeholder="Search">
+        </div>
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3"><i class="fa fa-search"></i></button>
+        </div>
     </form>
 
     <!-- Sign Out and Change Password Dropdown -->
     <div class="dropdown">
         <button class="dropbtn">Dropdown</button>
         <div class="dropdown-content">
-            <a><?= htmlspecialchars($_SESSION["username"]); ?></a>
-            <a href="/sql-user-authentication-app/src/include/logout.php">Sign Out of Your Account</a>
-            <a href="/sql-user-authentication-app/src/include/reset-password.php">Reset Your Password</a>
+            <a title="username"><?= htmlspecialchars($_SESSION["username"]); ?></a>
+            <a href="/sql-user-authentication-app/src/include/logout.php" title="Sign Out">Sign Out</a>
+            <a href="/sql-user-authentication-app/src/include/reset-password.php" title="Reset Password">Reset Password</a>
         </div>
     </div>
 </header>
