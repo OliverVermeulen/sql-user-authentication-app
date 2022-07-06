5f8532ac22e46d369463925325c2778bbdae2141
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-06 13:15:44 +0200

    added librarian sign in

---
+++ src/pages/librarian.php
@@ -0,0 +1,17 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: /sql-user-authentication-app/index.php");
+    exit;
+}
+
+// Include config file
+require_once "/MAMP/htdocs/sql-user-authentication-app/src/include/config.inc.php";
+$sql = "SELECT * FROM librarians WHERE username = librarian";
+
+$_SESSION["loggedin"] = true;
+$_SESSION["username"] = "Librarian";
+header("location: /sql-user-authentication-app/index.php");
