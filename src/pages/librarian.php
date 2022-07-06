aead72bdef076dcf350998ed8a08689e37083740
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-06 13:19:03 +0200

    Added librarian page

--- src/pages/librarian.php
+++ src/pages/librarian.php
@@ -4,7 +4,7 @@ session_start();
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: /sql-user-authentication-app/index.php");
+    header("location: /sql-user-authentication-app/src/pages/librarian-dashboard.php");
     exit;
 }
 
@@ -14,4 +14,4 @@ $sql = "SELECT * FROM librarians WHERE username = librarian";
 
 $_SESSION["loggedin"] = true;
 $_SESSION["username"] = "Librarian";
-header("location: /sql-user-authentication-app/index.php");
+header("location: /sql-user-authentication-app/src/pages/librarian-dashboard.php");
