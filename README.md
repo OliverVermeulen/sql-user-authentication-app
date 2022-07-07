3af1d023a9f53aa0928da714a29c76264a0057e7
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-07 19:05:59 +0200

    large save

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# sql-user-authentication-app
+# SQL User Authentication App
+
+## Description
+
 The aim of this project is to make a user authentication and library management app with PHP and SQL
+
+## Features
+
+1. The ability to sign in a a user and librarian
+2. The ability to search for a specific book or author
+3. The ability to sort data
+4. Librarian CRUD:
+    4.1 The ability to Create new books and authors
+    4.2 The ability to Read information about books and authors
+    4.3 The ability to Update information of books and authors
+    4.4 The ability to Delete books and authors
+
+
+## MVP
+
+- A Sign In, Sign Up, Update Password and Sign Out pages/functions
+- Tables for authors, books, users and librarians
+- A search bar
+- A sorting feature
+- Only be able to use CRUD while signed in as a librarian
+
+## Backlog
+
+- Add update function
+- Add search bar
+- Add sorting function
+
+## Future Additions
+
+- A dedicated Admin/Librarian sign in form
+- A live/real time search bar
+
+## Links
+
+### Repo link
+
+https://github.com/OliverVermeulen/php-oop-booking-app.git
--- src/librarian/librarian-dashboard.php
+++ src/librarian/librarian-dashboard.php
@@ -65,7 +65,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                                 echo "<td>" . $row['age_group'] . "</td>";
                                 echo "<td class='action-list'>";
                                 echo '<a href="/sql-user-authentication-app/src/librarian/librarian-read.php?id=' . $row['book_id'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye">read</span></a>';
-                                echo '<a href="/sql-user-authentication-app/src/librarian/create.php?id=' . $row['book_id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil">update</span></a>';
+                                echo '<a href="/sql-user-authentication-app/src/librarian/update.php?id=' . $row['book_id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil">update</span></a>';
                                 echo '<a href="/sql-user-authentication-app/src/librarian/delete.php?id=' . $row['book_id'] . '" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash">delete</span></a>';
                                 echo "</td>";
                                 echo "</tr>";
