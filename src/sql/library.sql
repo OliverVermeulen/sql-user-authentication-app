065f2546304145a4cfced87c8840006f87b28475
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-08 12:52:32 +0200

    Update library.sql

--- src/sql/library.sql
+++ src/sql/library.sql
@@ -1,3 +1,13 @@
+-- Adds in users table
+CREATE TABLE `users` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(50),
+  `password` varchar(255)
+);
+
+
+
+-- Adds in books table
 CREATE TABLE `books` (
   `book_id` int(11) PRIMARY KEY AUTO_INCREMENT,
   `book_name` varchar(50),
@@ -7,6 +17,7 @@ CREATE TABLE `books` (
   `author_id` int(11)
 );
 
+-- Adds in authors table
 CREATE TABLE `authors` (
   `author_name` varchar(50),
   `author_age` varchar(10),
@@ -14,11 +25,28 @@ CREATE TABLE `authors` (
   `author_id` int(11) PRIMARY KEY AUTO_INCREMENT
 );
 
+-- Links books and authors tables via foreign key
 ALTER TABLE `books` ADD FOREIGN KEY (`author_id`) REFERENCES `authors`(`author_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-INSERT INTO `authors` (`author_name`, `author_age`, `author_genre`, `author_id`) VALUES 
-(`Vikram Seth`, `68`, `Poet`, NULL),
-(`Abu'l-Fazl ibn Mubarak`, `deceased`, 'Biography', NULL),
-(`Phillip Zimbardo`, `87`, `Psychologist`, NULL),
-(`Jane Austin`, `deceased`, `Poet`, NULL),
-(`J.M Coetzee`, `81`, `Novelist`, NULL);
+-- Adds records into authors table
+INSERT INTO `authors` (`author_name`, `author_age`, `author_genre`, `author_id`) VALUES ('Vikram Seth', '68', 'Poet', NULL), ('Abul-Fazl ibn Mubarak', 'deceased', 'Biographer', NULL), ('Phillip Zimbardo', '87', 'Psychologist', NULL), ('Jane Austin', 'deceased', 'Novelist', NULL), ('J.M Coetzee', '81', 'Novelist', NULL);
+
+-- Adds records into books table
+INSERT INTO `books` (`book_id`, `book_name`, `release_year`, `book_genre`, `age_group`, `author_id`) VALUES 
+(NULL, 'The Tale Of Melon City', '1981', 'Poetry', '16', '1'), 
+(NULL, 'The Humble Administrators Garden', '1985', 'Poetry', '18', '1'),
+(NULL, 'All You Who sleep Tonight', '1990', 'Poetry', '18', '1'),
+(NULL, 'Akbarnama', '2011', 'Biography', '18', '2'),
+(NULL, 'The Cognitive Control of Motivation', '1969', 'Psychology', '18', '3'),
+(NULL, 'Stanford prison experiment', '1972', 'Psychology', '18', '3'),
+(NULL, 'Influencing Attitudes and Changing Behaviour', '1969', 'Psychology', '18', '3'),
+(NULL, 'Sence and Sensibility', '1811', 'Novel', '12', '4'),
+(NULL, 'Pride and Prejudice', '1813', 'Novel', '14', '4'),
+(NULL, 'Mansfield Park', '1814', 'Novel', '20-99', '4'),
+(NULL, 'Emma', '1815', 'Novel', '2-10', '4'),
+(NULL, 'Nothanger Abbey', '1818', 'Novel', '11-19', '4'),
+(NULL, 'Persuasion', '1818', 'Novel', '20-99', '4'),
+(NULL, 'Lady Susan', '1871', 'Novel', '20-99', '4'),
+(NULL, 'The Childhood of Jesus', '2013', 'Novel', '12-15', '5'),
+(NULL, 'The Schooldays of Jesus', '2016', 'Novel', '8-10', '5'),
+(NULL, 'The Death of Jesus', '2019', 'Novel', '12-17', '5')
